c43b80d8c2049a1c271f12cfe3fb0ea6f3f9840b
Jaret Wright <hkkA223>
2021-02-25 12:34:18 -0500

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,32 @@
+public class Rectangle {
+    private int length, width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getPerimeter(){
+        return 0;
+    }
+
+    public int getArea(){
+        return 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
